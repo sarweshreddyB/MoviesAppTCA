71d43d2e80524a718b1b02ade98a206bcd29692a
Sarwesh Reddy <sarwesh.reddy8600@gmail.com>
2024-04-29 23:21:50 +0530

    Added default project files

---
+++ MoviesAppTCA/MoviesAppTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTCAApp.swift
+//  MoviesAppTCA
+//
+//  Created by Sarwesh Boje on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
